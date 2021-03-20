62f2f82391be554ec501cfb43e7b8e42c408687d
Umakant Vashishtha <umakantvashishtha98@gmail.com>
2021-03-20 23:04:17 +0530

    API health check

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/tasks', tasks);
 app.use('/api/groups', groups);
+app.get('/ping', (req, res) => res.send("Pong"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
